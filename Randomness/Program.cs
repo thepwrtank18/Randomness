bfd9289c15832830cfd9d161e896d3e117b355a0
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2021-11-27 21:04:45 -0500

    Add project files.

---
+++ Randomness/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Note that the following code could be written by *anyone*.");
+Console.WriteLine("By continuing, you agree that you are responsible for anything ran.");
+Console.ReadKey();
+
+while (true)
+{
+    string code = 
+}

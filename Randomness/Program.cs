8b529ebf7d43a6e694990c1c3a18f458d4447b2b
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2021-11-27 21:11:15 -0500

    code

--- Randomness/Program.cs
+++ Randomness/Program.cs
@@ -1,9 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Note that the following code could be written by *anyone*.");
-Console.WriteLine("By continuing, you agree that you are responsible for anything ran.");
-Console.ReadKey();
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using System.Net;
 
-while (true)
+namespace Randomness
 {
-    string code = 
+    internal class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Note that the following code could be written by *anyone*.");
+            Console.WriteLine("You can find the code by going to https://github.com/thepwrtank18/Randomness/code.txt.");
+            Console.WriteLine("By continuing, you agree that you are responsible for anything ran.");
+            Console.ReadKey();
+
+            while (true)
+            {
+                WebClient client = new();
+                string code = client.DownloadString("https://raw.githubusercontent.com/thepwrtank18/Randomness/master/code.txt");
+
+                await CSharpScript.RunAsync(code);
+            }
+        }
+    }
 }
